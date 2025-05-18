ad480a7a73125aeb8d8c0eddc08aff301e02ac2b
Sesgaro <122136427+Sesgaro@users.noreply.github.com>
2025-05-17 18:22:06 -0600

    Update vesc_can_bus_arduino.h

--- vesc_can_bus_arduino.h
+++ vesc_can_bus_arduino.h
@@ -1,40 +1,33 @@
-#ifndef VESC_CAN_BUS_ARDUINO_H
-#define VESC_CAN_BUS_ARDUINO_H
+#include <Arduino.h> //required for PD# definitions
 
-#include <Arduino.h>
-#include <mcp_can.h>
-#include <SPI.h>
-
-class VescCAN {
-private:
-    MCP_CAN& CAN0;
-    uint8_t deviceId;
-    long unsigned int rxId;
-    unsigned char len;
-    unsigned char rxBuf[8];
+class CAN
+{
 
 public:
-    float dutyCycleNow;
-    float avgMotorCurrent;
-    float erpm;
-    float WattHours;
-    float tempFET;
-    float tempMotor;
-    float avgInputCurrent;
-    float inpVoltage;
-    void processMessage(unsigned long id, unsigned char* data);
 
-    VescCAN(MCP_CAN& can, uint8_t devId);
+#define CAN0_INT 21                              // Set INT to pin 21
 
-    void spin();
-    void vesc_set_duty(float duty);
-    void vesc_set_current(float current);
-    void vesc_set_erpm(float eerpm);
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];// Array to store serial string
 
-private:
-    void get_frame();
-    float process_data_frame_vesc(char datatype, unsigned char byte1, unsigned char byte2);
-    int hex2int(char buf[]);
+
+float inpVoltage, dutyCycleNow, avgInputCurrent, avgMotorCurrent, tempFET, tempMotor;
+long erpm, WattHours;
+
+
+void initialize();
+void spin();
+void get_frame(); // populates rxId and rxBuf with latest can frame 
+void can_send(byte data[8]); //transmits the send commands to the sensor
+void print_raw_can_data(); //output raw can data to terminal (debug)
+
+
+void vesc_set_duty(float duty);
+void vesc_set_current(float current);
+void vesc_set_erpm(float erpm);
+float process_data_frame_vesc(char datatype, unsigned char byte1, unsigned char byte2);
+int hex2int(char buf[]);
+
 };
-
-#endif
